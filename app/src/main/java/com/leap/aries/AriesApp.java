e82c8683b5a0b7ec67f6585f176e51fa040034ad
weiyaling <weiyaling@hd123.com>
2017-06-23 16:37:36 +0800

    mini

--- app/src/main/java/com/leap/aries/AriesApp.java
+++ app/src/main/java/com/leap/aries/AriesApp.java
@@ -1,13 +1,13 @@
 package com.leap.aries;
 
-import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 /**
  * AriesApp
  * Created by ylwei on 2017/6/23.
  */
 
-public class AriesApp extends Application {
+public class AriesApp extends MultiDexApplication {
 
   @Override
   public void onCreate() {
