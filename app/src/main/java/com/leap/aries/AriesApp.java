ff53a798e93465f3947dd5ef42417a7a8a8736d9
weiyaling <weiyaling@hd123.com>
2017-06-23 15:58:31 +0800

    no message

---
+++ app/src/main/java/com/leap/aries/AriesApp.java
@@ -0,0 +1,18 @@
+package com.leap.aries;
+
+import android.app.Application;
+
+/**
+ * AriesApp
+ * Created by ylwei on 2017/6/23.
+ */
+
+public class AriesApp extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+
+  }
+}
--- app/src/main/java/com/leap/aries/presentation/MainActivity.java
+++ app/src/main/java/com/leap/aries/presentation/MainActivity.java
@@ -1,7 +1,9 @@
-package com.leap.aries;
+package com.leap.aries.presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.leap.aries.R;
 
 public class MainActivity extends AppCompatActivity {
 
